ac5eaa1eceec89c9ef190c9ce26c453e742c3afc
sannith <sannith.bode@sstech.us>
2020-05-01 15:57:57 +0530

    no message

--- UI/Zoomdata_old/network.js
+++ UI/Zoomdata_old/network.js
@@ -1,90 +1,102 @@
 
-console.log(am4core)
-console.log(am4themes_animated)
+	console.log(am4core)
+	console.log(am4themes_animated)
 
 
-am4core.useTheme(am4themes_animated);
-// Themes end
+	am4core.useTheme(am4themes_animated);
+	// Themes end
 
 
-looker.plugins.visualizations.add({
-  create: function(element, config) {
-	  element.innerHTML = `
-      <style>
-		.sannith {
-		  width: 100%;
-		  max-width: 100%;
-		height:550px;
+	looker.plugins.visualizations.add({
+	  create: function(element, config) {
+		  element.innerHTML = `
+		  <style>
+			.sannith {
+			  width: 100%;
+			  max-width: 100%;
+			height:550px;
+			}
+		 </style>
+		`;
+
+		var container = element.appendChild(document.createElement("div"));
+		container.id = 'amContainer';
+		container.className = "sannith";
+	  },
+	 
+	  updateAsync: function(data, element, config, queryResponse, details, doneRendering) {
+		// Clear any errors from previous updates:
+		this.clearErrors();
+
+		// Dump data and metadata to console:
+		console.log('updateAsync() data', data)
+		console.log('updateAsync() config', config)
+		console.log('updateAsync() queryResponse', queryResponse)
+		
+		user_name = config.query_fields.dimensions[0].name;
+		login_id = config.query_fields.dimensions[1].name;
+		
+		var amData = [];
+		//var colorSet = new am4core.ColorSet();
+		for(var row of data) {
+			//var cell = row[queryResponse.fields.dimensions[3].name]
+			//xyz = LookerCharts.Utils.htmlForCell(cell);
+			amData.push({
+				name: row[user_name].value,
+			  children: [{name: 'Black Tea', value: 1}, { name: 'Floral',value: 1}]
+			});
+			
 		}
-	 </style>
-    `;
+		
+		
+	   
+		console.log('amChart data', amData)
+		
+		let groups = Object.create(null);
 
-    var container = element.appendChild(document.createElement("div"));
-    container.id = 'amContainer';
-	container.className = "sannith";
-  },
- 
-  updateAsync: function(data, element, config, queryResponse, details, doneRendering) {
-    // Clear any errors from previous updates:
-    this.clearErrors();
+	data.forEach(item => {
+		if (!groups[item.user_name]) {
+			groups[item.user_name] = [];
+		}
 
-    // Dump data and metadata to console:
-    console.log('updateAsync() data', data)
-    console.log('updateAsync() config', config)
-    console.log('updateAsync() queryResponse', queryResponse)
-	
-	user_name = config.query_fields.dimensions[0].name;
-    login_id = config.query_fields.dimensions[1].name;
-	
-	console.log('username',user_name)
-	
-	
-    const groupBy = key => array =>
-  array.reduce((objectsByKeyValue, obj) => {
-    const value = obj[key];
-    objectsByKeyValue[value] = (objectsByKeyValue[value] || []).concat(obj);
-    return objectsByKeyValue;
-  }, {});
-  
-    const groupByname = groupBy('user_name');
-	const groupByid = groupBy('login_id');
-	
-	
-    console.log('groupby',groupByname)
-	
-	console.log(
-  JSON.stringify({
-    name: groupByname(data),
-	id : groupByid(data)
-  }, null, 2)
-);
-	
-	//console.log('amChart data', amData)
-	
-	
-	
-let chart = am4core.create("amContainer", am4plugins_forceDirected.ForceDirectedTree);
-chart.legend = new am4charts.Legend();
-
-let networkSeries = chart.series.push(new am4plugins_forceDirected.ForceDirectedSeries())
+		groups[item.Level].push({
+			login_id: item.login_id
+		});
+	});
 
 
 
-networkSeries.dataFields.linkWith = "linkWith";
-networkSeries.dataFields.name = "name";
-networkSeries.dataFields.value = 1;
-networkSeries.dataFields.children = "children";
+	let result =
+		Object.entries(groups)
+			.map(([k, v]) => ({[k]: v}));
+			
+	console.log('testdata',result)
 
-networkSeries.nodes.template.tooltipText = "{name}";
-networkSeries.nodes.template.fillOpacity = 1;
 
-networkSeries.nodes.template.label.text = "{name}"
-networkSeries.fontSize = 8;
-networkSeries.maxLevels = 2;
-networkSeries.maxRadius = am4core.percent(6);
-networkSeries.manyBodyStrength = -16;
-networkSeries.nodes.template.label.hideOversized = true;
-networkSeries.nodes.template.label.truncate = true;
-doneRendering();
-}
-})
+		
+	let chart = am4core.create("amContainer", am4plugins_forceDirected.ForceDirectedTree);
+	chart.legend = new am4charts.Legend();
+
+	let amData = chart.series.push(new am4plugins_forceDirected.ForceDirectedSeries())
+
+
+
+	amData.dataFields.linkWith = "linkWith";
+	amData.dataFields.name = "name";
+	amData.dataFields.id = "name"
+	amData.dataFields.value = "value";
+	amData.dataFields.children = "children";
+
+	amData.nodes.template.tooltipText = "{name}";
+	amData.nodes.template.fillOpacity = 1;
+
+	amData.nodes.template.label.text = "{name}"
+	amData.fontSize = 8;
+	amData.maxLevels = 2;
+	amData.maxRadius = am4core.percent(6);
+	amData.manyBodyStrength = -16;
+	amData.nodes.template.label.hideOversized = true;
+	amData.nodes.template.label.truncate = true;
+	doneRendering();
+	}
+	})
