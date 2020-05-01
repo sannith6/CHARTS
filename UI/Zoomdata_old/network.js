6fee56c7448da5f53c5a5d78ab0def4891c4ddc4
sannith <sannith.bode@sstech.us>
2020-05-01 14:34:17 +0530

    no message

--- UI/Zoomdata_old/network.js
+++ UI/Zoomdata_old/network.js
@@ -64,7 +64,7 @@ let networkSeries = chart.series.push(new am4plugins_forceDirected.ForceDirected
 networkSeries.dataFields.linkWith = "linkWith";
 networkSeries.dataFields.name = "name";
 networkSeries.dataFields.id = "name";
-networkSeries.dataFields.value = "value";
+networkSeries.dataFields.value = 1;
 networkSeries.dataFields.children = "children";
 
 networkSeries.nodes.template.tooltipText = "{name}";
