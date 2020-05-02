114e1c9a906e7fada66aae89670ee9e28e6377d0
sannith <sannith.bode@sstech.us>
2020-05-02 13:02:19 +0530

    no message

--- UI/Zoomdata_old/network.js
+++ UI/Zoomdata_old/network.js
@@ -88,13 +88,13 @@
 	amData.nodes.template.tooltipText = "{name}";
 	amData.nodes.template.fillOpacity = 1;
 
-	amData.nodes.template.label.text = "{name}"
-	amData.fontSize = 8;
-	amData.maxLevels = 2;
-	amData.maxRadius = am4core.percent(6);
-	amData.manyBodyStrength = -16;
-	amData.nodes.template.label.hideOversized = true;
-	amData.nodes.template.label.truncate = true;
+	//amData.nodes.template.label.text = "{name}"
+	//amData.fontSize = 8;
+	//amData.maxLevels = 2;
+	//amData.maxRadius = am4core.percent(6);
+	//amData.manyBodyStrength = -16;
+	//amData.nodes.template.label.hideOversized = true;
+	//amData.nodes.template.label.truncate = true;
 	doneRendering();
 	}
 	})
