0b1ddfb68adeca744f04ce83f73a860cc8aa1e6b
sannith <sannith.bode@sstech.us>
2020-05-04 14:01:42 +0530

    no message

--- UI/Zoomdata_old/network.js
+++ UI/Zoomdata_old/network.js
@@ -86,10 +86,10 @@ amData.dataFields.id = "name";
 amData.dataFields.value = "value";
 amData.dataFields.children = "children";
 
-amData.nodes.template.tooltipHTML = name;
+  amData.nodes.template.tooltipHTML = "{name}";
 amData.nodes.template.fillOpacity = 1;
 
-amData.nodes.template.label.text = name
+  amData.nodes.template.label.text = "{name}"
 amData.tooltip.label.interactionsEnabled = true;
 amData.tooltip.keepTargetHover = true;
 amData.fontSize = 8;
