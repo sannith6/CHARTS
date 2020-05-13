b77a7393f1517707b06573e7a843e7bcf1ecf590
svanitha95 <anitha.singamsetti@sstech.us>
2020-05-13 22:37:08 +0530

    modified anitha.js file

--- UI/Zoomdata_old/anitha.js
+++ UI/Zoomdata_old/anitha.js
@@ -229,7 +229,7 @@ looker.plugins.visualizations.add({
 		  response.push({
 			
 			// "IP ADDRESS": "johnwhite" ,
-			"IP ADDRESS": item[1].name,
+			"IP ADDRESS": item[name],
 			"SPARKLINE": {
 			  "date": "2020-02-29,2020-03-05,2020-03-22,2020-03-26,2020-03-27".split(','),
 			  "score": "82.5806,49.6353,78.3649,29.2346,47.3802".split(',').map(s => parseInt(s))
