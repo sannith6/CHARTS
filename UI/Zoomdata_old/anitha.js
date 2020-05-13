ddfbf8e7355b05386f1b2b127772f12f0c83c8bd
svanitha95 <anitha.singamsetti@sstech.us>
2020-05-13 21:50:35 +0530

    modified anitha.js file

--- UI/Zoomdata_old/anitha.js
+++ UI/Zoomdata_old/anitha.js
@@ -83,18 +83,11 @@ looker.plugins.visualizations.add({
         
       </style>
     `;
-    // var container = element.appendChild(document.createElement("div"));
-	// container.className = "sannith";
-    // container.id = 'amContainer';
-	
-	
+
 	var chartContainer = element.appendChild(document.createElement("div"));
 	chartContainer.className = 'chart-container';
 	chartContainer.id = 'chartContainer';
 
-	
-
-	//this.container = element.appendChild(document.createElement("div"));
 	
   },
 
@@ -111,9 +104,7 @@ looker.plugins.visualizations.add({
 	date = config.query_fields.dimensions[0].name;
     name = config.query_fields.dimensions[1].name;
 	score = config.query_fields.dimensions[2].name;
-	// dst_name = config.query_fields.dimensions[2].name;
-	// userid = config.query_fields.dimensions[3].name;
-	
+
 	var amData = [];
 	// var colorSet = new am4core.ColorSet();
     for(var row of data) {
@@ -128,27 +119,29 @@ looker.plugins.visualizations.add({
 		
     }
 	
+	console.log("-------------------------------");
 	console.log('amChart data', amData)
 	
-	// var amData = [];
-	// // var colorSet = new am4core.ColorSet();
-    // for(var row of data) {
-		// var cell = row[queryResponse.fields.dimensions[1].name]
-		// xyz = LookerCharts.Utils.htmlForCell(cell);
-        // amData.push({
-            // category: row[scores].value,
-			// start: row[start_date].value,
-			// end : row[end_date].value,
-			// color: colorSet.next() ,
-			// //task: row[userid].value,
-			// task: xyz
-        // });
-		
-    // }
 	
+	var amData1 = [];
+	// var colorSet = new am4core.ColorSet();
+    for(var row of data) {
+		var cell = row[queryResponse.fields.dimensions[1].name]
+		xyz = LookerCharts.Utils.htmlForCell(cell);
+        amData1.push({
+            row[date].value,
+			row[name].value,
+			row[score].value
+			
+        });
+		
+    }
+	
+	console.log("-------------------------------");
+	console.log('amChart data1', amData1)
 	
 	function chartChart(response) {
-	  // var echarts = require('echarts');
+	
 	  response.map((amData, i) => {
 		var neCustomchart = document.createElement('div');
 		neCustomchart.style.height = '80px';
@@ -182,8 +175,7 @@ looker.plugins.visualizations.add({
 	}
 	
 	function createChartTable(response) {
-  // console.log(response, "chartResponse")
-		// var echarts = require('echarts');
+ 
 	  if (response.length !== 0) {
 
 		var headers = ''
@@ -229,7 +221,7 @@ looker.plugins.visualizations.add({
 
 		chartContainer.innerHTML = view
 		chartChart(response)
-		addOnClickHandler()
+		
 	  }
 	}
 
@@ -258,160 +250,7 @@ looker.plugins.visualizations.add({
 		console.log('response data is------------',response);
 		createChartTable(response)
 	  }
-	
 
-	
-
-    // // get the names of the first dimension and measure available in data
-    // start_date = config.query_fields.dimensions[0].name;
-    // end_date = config.query_fields.dimensions[1].name;
-	// dst_name = config.query_fields.dimensions[2].name;
-	// userid = config.query_fields.dimensions[3].name;
-	
-	
-    // // build data array for the chart, by iterating over the Looker data object
-    // var amData = [];
-	// var colorSet = new am4core.ColorSet();
-    // for(var row of data) {
-		// var cell = row[queryResponse.fields.dimensions[3].name]
-		// xyz = LookerCharts.Utils.htmlForCell(cell);
-        // amData.push({
-            // category: row[dst_name].value,
-			// start: row[start_date].value,
-			// end : row[end_date].value,
-			// color: colorSet.next() ,
-			// task: row[userid].value,
-			// task: xyz
-        // });
-		
-    // }
-	
-	// element.innerHTML = xyz;
-	
-	// console.log('amChart data', amData)
-
-
-
-
-	// let chart = am4core.create("amContainer", am4plugins_timeline.SerpentineChart);
-	// chart.curveContainer.padding(50, 20, 50, 20);
-	// chart.levelCount = 4;
-	// chart.yAxisRadius = am4core.percent(25);
-	// chart.yAxisInnerRadius = am4core.percent(-25);
-	// chart.maskBullets = false;
-
-	// colorSet.saturation = 0.5;
-
-	// chart.data = amData;
-
-	// chart.dateFormatter.dateFormat = "yyyy-MM-dd HH";
-	// chart.dateFormatter.inputDateFormat = "yyyy-MM-dd HH";
-	// chart.fontSize = 11;
-
-	// let categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
-	// categoryAxis.dataFields.category = "category";
-	// categoryAxis.renderer.grid.template.disabled = true;
-	// categoryAxis.renderer.labels.template.paddingRight = 25;
-	// categoryAxis.renderer.minGridDistance = 10;
-	// categoryAxis.renderer.innerRadius = -60;
-	// categoryAxis.renderer.radius = 60;
-	// categoryAxis.labelsEnabled = false;
-
-	// let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-	// dateAxis.renderer.minGridDistance = 70;
-	// dateAxis.baseInterval = { count: 1, timeUnit: "hour" };
-	// dateAxis.renderer.tooltipLocation = 0;
-	// dateAxis.startLocation = -0.5;
-	// dateAxis.renderer.line.strokeDasharray = "1,4";
-	// dateAxis.renderer.line.strokeOpacity = 0.6;
-	// dateAxis.tooltip.background.fillOpacity = 0.2;
-	// dateAxis.tooltip.background.cornerRadius = 5;
-	// dateAxis.tooltip.label.fill = new am4core.InterfaceColorSet().getFor("alternativeBackground");
-	// dateAxis.tooltip.label.paddingTop = 7;
-
-	// let labelTemplate = dateAxis.renderer.labels.template;
-	// labelTemplate.verticalCenter = "middle";
-	// labelTemplate.fillOpacity = 0.7;
-	// labelTemplate.background.fill = new am4core.InterfaceColorSet().getFor("background");
-	// labelTemplate.background.fillOpacity = 1;
-	// labelTemplate.padding(7, 7, 7, 7);
-
-	// let series = chart.series.push(new am4plugins_timeline.CurveColumnSeries());
-	// series.columns.template.height = am4core.percent(20);
-	// series.columns.template.tooltipText = "{task}: [bold]{openDateX}[/] - [bold]{dateX}[/]";
-	// series.columns.template.tooltipHTML = '{task}: <b>{openDateX}:00</b> - <b>{dateX}:00</b>';
-        // series.tooltip.label.interactionsEnabled = true;
-        // series.tooltip.keepTargetHover = true;
-
-	// series.dataFields.openDateX = "start";
-	// series.dataFields.dateX = "end";
-	// series.dataFields.categoryY = "category";
-	// series.columns.template.propertyFields.fill = "color"; // get color from data
-	// series.columns.template.propertyFields.stroke = "color";
-	// series.columns.template.strokeOpacity = 0;
-
-	// let bullet = series.bullets.push(new am4charts.CircleBullet());
-	// bullet.circle.radius = 3;
-	// bullet.circle.strokeOpacity = 0;
-	// bullet.propertyFields.fill = "color";
-	// bullet.locationX = 0;
-
-
-	// let bullet2 = series.bullets.push(new am4charts.CircleBullet());
-	// bullet2.circle.radius = 3;
-	// bullet2.circle.strokeOpacity = 0;
-	// bullet2.propertyFields.fill = "color";
-	// bullet2.locationX = 1;
-
-
-	// let imageBullet1 = series.bullets.push(new am4plugins_bullets.PinBullet());
-	// imageBullet1.disabled = true;
-	// imageBullet1.propertyFields.disabled = "disabled1";
-	// imageBullet1.locationX = 1;
-	// imageBullet1.circle.radius = 20;
-	// imageBullet1.propertyFields.stroke = "color";
-	// imageBullet1.background.propertyFields.fill = "color";
-	// imageBullet1.image = new am4core.Image();
-	// imageBullet1.image.propertyFields.href = "image1";
-
-	// let imageBullet2 = series.bullets.push(new am4plugins_bullets.PinBullet());
-	// imageBullet2.disabled = true;
-	// imageBullet2.propertyFields.disabled = "disabled2";
-	// imageBullet2.locationX = 0;
-	// imageBullet2.circle.radius = 20;
-	// imageBullet2.propertyFields.stroke = "color";
-	// imageBullet2.background.propertyFields.fill = "color";
-	// imageBullet2.image = new am4core.Image();
-	// imageBullet2.image.propertyFields.href = "image2";
-
-   
-	// let eventSeries = chart.series.push(new am4plugins_timeline.CurveLineSeries());
-	// eventSeries.dataFields.dateX = "eventDate";
-	// eventSeries.dataFields.categoryY = "category";
-	// eventSeries.data = [
-		// { category: "", eventDate: "openDateX", letter: "", description: "" },
-		// { category: "", eventDate: "dateX", letter: "", description: "" }];
-	// eventSeries.strokeOpacity = 0;
-
-	// let flagBullet = eventSeries.bullets.push(new am4plugins_bullets.FlagBullet())
-	// flagBullet.label.propertyFields.text = "letter";
-	// flagBullet.locationX = 0;
-	// flagBullet.tooltipText = "{description}";
-
-	// chart.scrollbarX = new am4core.Scrollbar();
-	// chart.scrollbarX.align = "center"
-	// chart.scrollbarX.width = am4core.percent(85);
-
-	// let cursor = new am4plugins_timeline.CurveCursor();
-	// chart.cursor = cursor;
-	// cursor.xAxis = dateAxis;
-	// cursor.yAxis = categoryAxis;
-	// cursor.lineY.disabled = true;
-	// cursor.lineX.strokeDasharray = "1,4";
-	// cursor.lineX.strokeOpacity = 1;
-
-	// dateAxis.renderer.tooltipLocation2 = 0;
-	// categoryAxis.cursorTooltipEnabled = false;
 
 	doneRendering();
 }
