dae50e018208a366d98ceecc86404449c743f830
svanitha95 <anitha.singamsetti@sstech.us>
2020-07-15 16:16:49 +0530

    css changes

--- UI/Zoomdata_old/anitha.js
+++ UI/Zoomdata_old/anitha.js
@@ -165,7 +165,7 @@ background-color: #fff;
 		var view = `
 			   <div>
 					<table class="table tabeldash" id="usertable_id" style="width:100%;height:100%;border-spacing: 5px;">
-						<thead style="color: rgba(0,0,0,0.54);font-size: small;font-weight: bold;position: fixed;">
+						<thead style="color: rgba(0,0,0,0.54);font-size: small;font-weight: bold;">
 							<tr class="tableHead">
 							${headers}
 							</tr>
