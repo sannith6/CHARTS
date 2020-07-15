4b0f42ffe6c07003ad6b698622009dc9a1d74539
svanitha95 <anitha.singamsetti@sstech.us>
2020-07-15 17:13:23 +0530

    css changes

--- UI/Zoomdata_old/anitha.js
+++ UI/Zoomdata_old/anitha.js
@@ -52,14 +52,20 @@ color: #3a4245;
 background-color: #fff;
 }
 
+table thead tr{
+    display:block;
+}
+
+table th,table td{
+    width:100%;//fixed width
+}
 
 
 table  tbody{
-  
-  display : block;
+  display:block;
+  height:200%;
   overflow:auto;//set tbody to auto
 }
-
 
    </style>
 
