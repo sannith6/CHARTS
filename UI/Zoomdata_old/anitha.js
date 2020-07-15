61cc696e51f3c5cdb49c0aba5d4b7c88382ba2fe
svanitha95 <anitha.singamsetti@sstech.us>
2020-07-15 16:33:02 +0530

    css changes

--- UI/Zoomdata_old/anitha.js
+++ UI/Zoomdata_old/anitha.js
@@ -15,6 +15,7 @@ looker.plugins.visualizations.add({
 	  element.innerHTML = `
       <style>
 		.chart-container {
+    position:fixed;			
     width: 100%;
     height: 100%;
     overflow-y: auto;
@@ -165,7 +166,7 @@ background-color: #fff;
 		var view = `
 			   <div>
 					<table class="table tabeldash" id="usertable_id" style="width:100%;height:100%;border-spacing: 5px;">
-						<thead style="color: rgba(0,0,0,0.54);font-size: small;font-weight: bold;position:fixed;">
+						<thead style="color: rgba(0,0,0,0.54);font-size: small;font-weight: bold;">
 							<tr class="tableHead">
 							${headers}
 							</tr>
