adc4812fb7a342935194cc6fa287ddc94d803bfc
Sstech@369 <sannith6>
2020-07-18 08:17:15 -0700

    M

--- UI/Zoomdata_old/anitha_test_spark.js
+++ UI/Zoomdata_old/anitha_test_spark.js
@@ -186,7 +186,7 @@ background-color: #fff;
 
 		var view = `
 			   <div >
-					<table class="fixed_header" id="usertable_id" style="width:100%;height:100%;border-spacing: 5px;" >
+					<table class="fixed_header tabeldash" id="usertable_id" style="width:100%;height:100%;border-spacing: 5px;" >
 						<thead style="color: rgba(0,0,0,0.54);font-size: small;font-weight: bold;" id="top">
 							<tr class="tableHead" >
 							${headers}
