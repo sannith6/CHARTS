b1722c8213b7bae4a45f8c0aa7524d101daeddb5
svanitha95 <anitha.singamsetti@sstech.us>
2020-08-25 15:29:41 +0530

    new changes

--- UI/Zoomdata_old/anitha_new_timeline.js
+++ UI/Zoomdata_old/anitha_new_timeline.js
@@ -86,20 +86,8 @@ looker.plugins.visualizations.add({
 					};
 			});
 			
-		function myFunction() {
-			var dots = document.getElementById("dots");
-			var moreText = document.getElementById("more");
-			var btnText = document.getElementById("myBtn");
-
-			if (dots.style.display === "none") {
-			dots.style.display = "inline";
-			btnText.innerHTML = "Read more";
-			moreText.style.display = "none";
-			} else {
-			dots.style.display = "none";
-			btnText.innerHTML = "Read less";
-			moreText.style.display = "inline";
-			}
+		myFunction: function() {
+			console.log("entered into a function");
 			}			
 
 		Highcharts.chart("container", {
