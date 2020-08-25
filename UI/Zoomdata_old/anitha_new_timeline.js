fc68ea1410bcc9e77e03a9172735368ba98a694e
svanitha95 <anitha.singamsetti@sstech.us>
2020-08-25 15:07:23 +0530

    new changes

--- UI/Zoomdata_old/anitha_new_timeline.js
+++ UI/Zoomdata_old/anitha_new_timeline.js
@@ -28,25 +28,7 @@ looker.plugins.visualizations.add({
  
 
 	 
-	function myfunction(id, dots, btn){
-		console.log('entered here');
-		console.log(id);
-		console.log(dots);
-		console.log(btn);
-		var dots = document.getElementById(dots);
-		var moreText = document.getElementById(id);
-		var btnText = document.getElementById(btn);
 
-		if (dots.style.display === "none") {
-			dots.style.display = "inline";
-			btnText.innerHTML = "Read more";
-			moreText.style.display = "none";
-		} else {
-			dots.style.display = "none";
-			btnText.innerHTML = "Read less";
-			moreText.style.display = "inline";
-		}
-	}
   
    updateAsync: function(data, element, config, queryResponse, details, doneRendering) {
     // Clear any errors from previous updates:
@@ -121,7 +103,25 @@ looker.plugins.visualizations.add({
 			
 
 		
+		function myfunction(id, dots, btn){
+			console.log('entered here');
+			console.log(id);
+			console.log(dots);
+			console.log(btn);
+			var dots = document.getElementById(dots);
+			var moreText = document.getElementById(id);
+			var btnText = document.getElementById(btn);
 
+			if (dots.style.display === "none") {
+				dots.style.display = "inline";
+				btnText.innerHTML = "Read more";
+				moreText.style.display = "none";
+			} else {
+				dots.style.display = "none";
+				btnText.innerHTML = "Read less";
+				moreText.style.display = "inline";
+			}
+		}
 
 
 		Highcharts.chart('container', {
