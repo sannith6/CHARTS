4d5be68a2ee28ace4e45ede4cdf33a5c1802a9cd
svanitha95 <anitha.singamsetti@sstech.us>
2020-11-11 23:41:33 +0530

    new changes entirely

--- UI/Zoomdata_old/new_timelines_testing/recent_timeline.js
+++ UI/Zoomdata_old/new_timelines_testing/recent_timeline.js
@@ -64,16 +64,16 @@ looker.plugins.visualizations.add({
 			}
 			
 			.timeline ul li div {
-			  position: relative;
-			  bottom: 0;
-			  width: 400px;
-			  padding: 15px;
-			  background: #fff;
-				border: 1px solid #e7e7f0;
-				box-shadow: 1px 1px 1px 1px #e7e7f0;
+				position: relative;
+				bottom: 0;
+				width: 400px;
+				padding: 15px;
+				background: #fff;
+				border: 4px solid #e7e7f0;
+				box-shadow: 1px 0px 1px 0px #e7e7f0;
 				border-radius: 10px;
 			}
-			
+						
 			
 			.timeline ul li div::before {
 			  content: '';
