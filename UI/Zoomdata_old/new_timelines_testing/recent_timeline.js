078ad4904d77a0dfc4f159903bdc6140acd88ac5
svanitha95 <anitha.singamsetti@sstech.us>
2020-11-11 21:37:59 +0530

    css changes added by raghu

--- UI/Zoomdata_old/new_timelines_testing/recent_timeline.js
+++ UI/Zoomdata_old/new_timelines_testing/recent_timeline.js
@@ -337,14 +337,27 @@ looker.plugins.visualizations.add({
 		}
 		
 		.timeline ul li div {
-    position: relative;
-    bottom: 0;
-    width: 400px;
-    padding: 15px;
+			position: relative;
+			bottom: 0;
+			width: 400px;
+			padding: 15px;
+			background: #fff;
+			border: 4px solid #e7e7f0;
+			box-shadow: 1px 0px 1px 0px #e7e7f0;
+			border-radius: 10px;
+		}
+		
+		.timeline ul li:nth-child(odd) div::before {
+    left: -38px;
+    /* border-width: 8px 16px 8px 0; */
+    border-color: transparent transparent transparent transparent;
+    right: 100%;
+    bottom: 59px;
+    width: 39px;
+    height: 10px;
     background: #fff;
-    border: 4px solid #e7e7f0;
-    box-shadow: 1px 0px 1px 0px #e7e7f0;
-    border-radius: 10px;
+    border-top: 3px solid #e7e7f0;
+    border-bottom: 3px solid #e7e7f0;
 }
 
 
@@ -519,23 +532,23 @@ looker.plugins.visualizations.add({
 			
 			if(check_score_type == "MEDIUM"){
 				var data_color = "#f7a35c";
-				$("#test1").append("<li class='in-view1'><div><time>"+"<img src='"+image_type+"' height=20/>"+"<time style='font-family:Roboto;color:#71758f;font-size:12pt;float:right;'>"+str_data+'</time></time>'+"<p style='font-family:Roboto;color:#71758f;font-size:12pt'>Data Event Type: "+item.logintype+"</p><br><span class='toggle-text' style='display:none;font-family:Roboto;color:#71758f;font-size:12pt'>"+result+"</span>"+"<br/><a href='#'  class='toggle-text-button' style='font-family:Roboto;color:#417deb;font-size:12pt'>For More Information<i class='fas fa-chevron-down' style='font-size:12pt'></i></a>"+'</div></li>');
+				$("#test1").append("<li class='in-view1'><div><time>"+"<img src='"+image_type+"' height=20/>"+"<time style='color:#71758f;font-size:12pt;float:right;'>"+str_data+'</time></time>'+"<p style='color:#71758f;font-size:12pt'>Data Event Type: "+item.logintype+"</p><br><span class='toggle-text' style='display:none;color:#71758f;font-size:12pt'>"+result+"</span>"+"<br/><a href='#'  class='toggle-text-button' style='color:#417deb;font-size:12pt'>For More Information<i class='fas fa-chevron-down' style='font-size:12pt'></i></a>"+'</div></li>');
 				
 			}
 			else if(check_score_type == "HIGH"){
 				var data_color = "#ee0340";
 				
-				$("#test1").append("<li class='in-view2'><div><time>"+"<img src='"+image_type+"' height=20/>"+"<time style='font-family:Roboto;color:#71758f;font-size:12pt;float:right;'>"+str_data+'</time></time>'+"<p style='font-family:Roboto;color:#71758f;font-size:12pt'>Data Event Type: "+item.logintype+"</p><br><span class='toggle-text' style='display:none;font-family:Roboto;color:#71758f;font-size:12pt'>"+result+"</span>"+"<br/><a href='#'  class='toggle-text-button' style='font-family:Roboto;color:#417deb;font-size:12pt'>For More Information<i class='fas fa-chevron-down' style='font-size:12pt'></i></a>"+'</div></li>');
+				$("#test1").append("<li class='in-view2'><div><time>"+"<img src='"+image_type+"' height=20/>"+"<time style='color:#71758f;font-size:12pt;float:right;'>"+str_data+'</time></time>'+"<p style='color:#71758f;font-size:12pt'>Data Event Type: "+item.logintype+"</p><br><span class='toggle-text' style='display:none;color:#71758f;font-size:12pt'>"+result+"</span>"+"<br/><a href='#'  class='toggle-text-button' style='color:#417deb;font-size:12pt'>For More Information<i class='fas fa-chevron-down' style='font-size:12pt'></i></a>"+'</div></li>');
 			}
 			else if(check_score_type == "LOW"){
 				var data_color = "#0dc363";
-				$("#test1").append("<li class='in-view3'><div><time>"+"<img src='"+image_type+"' height=20/>"+"<time style='font-family:Roboto;color:#71758f;font-size:12pt;float:right;'>"+str_data+'</time></time>'+"<p style='font-family:Roboto;color:#71758f;font-size:12pt'>Data Event Type: "+item.logintype+"</p><br><span class='toggle-text' style='display:none;font-family:Roboto;color:#71758f;font-size:12pt'>"+result+"</span>"+"<br/><a href='#'  class='toggle-text-button' style='font-family:Roboto;color:#417deb;font-size:12pt'>For More Information<i class='fas fa-chevron-down' style='font-size:12pt'></i></a>"+'</div></li>');
+				$("#test1").append("<li class='in-view3'><div><time>"+"<img src='"+image_type+"' height=20/>"+"<time style='color:#71758f;font-size:12pt;float:right;'>"+str_data+'</time></time>'+"<p style='color:#71758f;font-size:12pt'>Data Event Type: "+item.logintype+"</p><br><span class='toggle-text' style='display:none;color:#71758f;font-size:12pt'>"+result+"</span>"+"<br/><a href='#'  class='toggle-text-button' style='color:#417deb;font-size:12pt'>For More Information<i class='fas fa-chevron-down' style='font-size:12pt'></i></a>"+'</div></li>');
 				
 			}
 			else if(check_score_type == "VERY LOW"){
 				var data_color = "#0e7ff2";
 				
-				$("#test1").append("<li class='in-view4'><div><time>"+"<img src='"+image_type+"' height=20/>"+"<time style='font-family:Roboto;color:#71758f;font-size:12pt;float:right;'>"+str_data+'</time></time>'+"<p style='font-family:Roboto;color:#71758f;font-size:12pt'>Data Event Type: "+item.logintype+"</p><br><span class='toggle-text' style='display:none;font-family:Roboto;color:#71758f;font-size:12pt'>"+result+"</span>"+"<br/><a href='#'  class='toggle-text-button' style='font-family:Roboto;color:#417deb;font-size:12pt'>For More Information<i class='fas fa-chevron-down' style='font-size:12pt'></i></a>"+'</div></li>');
+				$("#test1").append("<li class='in-view4'><div><time>"+"<img src='"+image_type+"' height=20/>"+"<time style='color:#71758f;font-size:12pt;float:right;'>"+str_data+'</time></time>'+"<p style='color:#71758f;font-size:12pt'>Data Event Type: "+item.logintype+"</p><br><span class='toggle-text' style='display:none;color:#71758f;font-size:12pt'>"+result+"</span>"+"<br/><a href='#'  class='toggle-text-button' style='color:#417deb;font-size:12pt'>For More Information<i class='fas fa-chevron-down' style='font-size:12pt'></i></a>"+'</div></li>');
 			}
 			else{
 				console.log("no color is defined for this score");
